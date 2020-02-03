f3f1e0d7c9d531630f8090bf14d9b884664bcdc4
Aakash Gajjar <skyqutip@gmail.com>
2020-02-03 20:17:01 +0530

    [fix] remove redundant code

--- download.rb
+++ download.rb
@@ -1,11 +1,6 @@
 #!/usr/bin/ruby
 # frozen_string_literal: true
 
-# @Author: Sky
-# @Date:   2019-01-28 14:40:35
-# @Last Modified by:   Sky
-# @Last Modified time: 2019-09-08 19:36:18
-
 require 'net/http'
 require 'uri'
 require 'json'
@@ -30,28 +25,12 @@ length = users.length
 users.each_with_index do |user, index|
   next if !user['download'].nil? && !user['download']
 
-  puts "Downloading User => [#{index}/#{length}] #{user['username']}, #{user['fullname']}"
+  status = "[#{index}/#{length}]"
+  userinfo = "#{user['username']}, #{user['fullname']}"
+  puts "Downloading User => #{status} #{userinfo}"
   request('/api/v2/tiktok/download',
           'id' => user['id'],
           'download_all' => user['download_all'],
           'url' => "https://www.tiktok.com/#{user['username']}")
   sleep(120)
 end
-
-# puts "Downloading media files"
-# #sleep(260)
-# system([
-#          "aria2c",
-#          "-q",
-#          "-x16",
-#          "-s16",
-#          "--user-agent=\"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.65 Safari/537.36\"",
-#          "--auto-file-renaming=false",
-#          "--continue=true",
-#          "-i /media/drive/b/Scripts/tiktok/urls_new.txt"
-# ].join(" "))
-#
-# puts "Finish downloading media files"
-
-# system("copy urls_new.txt #{Time.now.strftime('%Y-%m-%d_%H-%M-%S')}_urls_new.txt")
-# system("del urls_new.txt")
